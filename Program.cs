7235686abe0924f1a71471be515e32047319df89
IAmMoltony <koronavirusnyj@gmail.com>
2024-04-06 22:28:03 +0500

    Committee

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CSharpDocs;
+
+/// <summary>
+/// Program class
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// Say hello to someone
+    /// </summary>
+    ///
+    /// <param name="who">
+    /// Who to say hello to
+    /// </param>
+    public static void SayHello(string who)
+    {
+        Console.WriteLine($"Hey {who} how ya doin");
+    }
+
+    /// <summary>
+    /// Program entry point
+    /// </summary>
+    ///
+    /// <param name="args">
+    /// Command-line arguments
+    /// </param>
+    static void Main(string[] args)
+    {
+        SayHello("Jamie");
+    }
+}
